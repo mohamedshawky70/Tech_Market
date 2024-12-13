9517dca3328d6649c8794a75e371d8775cab2fe5
MO <57459035+mohamedshawky70@users.noreply.github.com>
2024-12-13 20:38:59 +0200

    Delete Models directory

--- Models/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyShop.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
